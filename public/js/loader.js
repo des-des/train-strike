5296dd1bc38f6e7cc284dc7cd7af37fb4845b64a
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-07-31 00:25:39 +0100

    final tweeks!

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,6 +1,6 @@
 [
   'https://www.gstatic.com/firebasejs/4.2.0/firebase.js',
-  'http://wzrd.in/standalone/uuid%2Fv4@latest',
+  'https://wzrd.in/standalone/uuid%2Fv4@latest',
   'js/main.js'
 ].forEach(function(src) {
   var script = document.createElement('script');
