569ac9ef29eab1842d94dcf2876975d8de440f0c
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-07-31 11:31:00 +0100

    moves uuid onto src

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,6 +1,6 @@
 [
   'https://www.gstatic.com/firebasejs/4.2.0/firebase.js',
-  'https://wzrd.in/standalone/uuid%2Fv4@latest',
+  'js/uuidv4.js',
   'js/main.js'
 ].forEach(function(src) {
   var script = document.createElement('script');
